61421a6ccaa9853e689adbab7cad989ac56d53e1
yunqi <yunqi-zhang@outlook.com>
2024-11-30 00:20:06 +0800

    text change

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
-  import moment from "moment";
-  import {photos} from "./data.ts";
-  var a = moment("20241117", "YYYYMMDD");
+import moment from "moment";
+import {photos} from "./data.ts";
+
+var a = moment("20241117", "YYYYMMDD");
   var b = moment();
   const formattedTime = moment.duration(b.diff(a)).days()
 </script>
 
 <template>
   <div class="absolute min-h-screen w-full bg-pink-50 flex flex-col items-center justify-center">
-        <h1 class="text-center font-bold text-3xl text-pretty-50">张云起和布热比耶在{{formattedTime}}天前相识</h1>
+        <h1 class="text-center font-bold text-3xl text-pretty-50">张云起和布热比耶在一起{{formattedTime}}天啦！</h1>
     <div class="container mx-auto">
       <h2 class="w-full text-left text-2xl m-5 font-bold">我们的点点滴滴:</h2>
       <div class="grid grid-cols-6 gap-8 ">
