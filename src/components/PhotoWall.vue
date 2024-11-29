f3f1495655b5881993b3deb2bfa1e6651fe07650
yunqi <yunqi-zhang@outlook.com>
2024-11-29 23:43:36 +0800

    first commit

---
+++ src/components/PhotoWall.vue
@@ -0,0 +1,8 @@
+<template>
+  <img v-lazy="photo.src" :alt="photo.title" />
+
+</template>
+
+<script setup>
+
+</script>
---
+++ src/data.ts
@@ -0,0 +1,13 @@
+export const photos = [
+    { id: 1, src: "/public/Image_61161263563646.jpg"},
+    { id: 2, src: '/public/Image_64892945605816.jpg'},
+    { id: 3, src: '/public/Image_64894347890451.jpg'},
+    { id: 4, src: '/public/Image_64896288322690.jpg'},
+    { id: 5, src: '/public/Image_64897617452116.jpg'},
+    { id: 6, src: '/public/Image_64907296548258.jpg'},
+    { id: 7, src: '/public/Image_64908844858935.jpg'},
+    { id: 8, src: '/public/Image_64910438463518.jpg'},
+    { id: 9, src: 'public/IMG_20240712_154024.jpg'},
+    { id: 10, src: 'public/IMG_20240427_131104.jpg'},
+    { id: 11, src: 'public/IMG_20240403_193829.jpg'},
+];
